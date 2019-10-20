79b2ff6578b7c247cbe7433d7fa85609413fa811
dickwyn <dickwyn@outlook.com>
2019-10-20 16:19:25 -0700

    (feat) initial layout for footer credits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>© {new Date().getFullYear()}, Built by dickwyn</footer>;
+  const date = new Date();
+
+  return (
+    <footer>
+      <div className="credits">
+        <p>
+          Arizona State University © Coalition of International Students{" "}
+          {date.getFullYear()}. All rights reserved
+        </p>
+        <p>
+          Built by{" "}
+          <a
+            href="https://www.dickwyn.xyz"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            dickwyn
+          </a>{" "}
+          and the collaborators. This code is available under the{" "}
+          <a
+            href="https://github.com/asucis/v3/blob/dev/LICENSE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            MIT License
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

37a36500c8d04b8d1447409c6393984c8bd26dd2
dickwyn <dickwyn@outlook.com>
2019-10-20 16:45:36 -0700

    (feat) initial structure of footer top section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,50 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const Footer = () => {
   const date = new Date();
 
+  const links = [
+    { id: 1, name: "link1", url: "https://www.google.com" },
+    { id: 2, name: "link2", url: "https://www.google.com" },
+    { id: 3, name: "link3", url: "https://www.google.com" },
+    { id: 4, name: "link4", url: "https://www.google.com" },
+    { id: 5, name: "link5", url: "https://www.google.com" },
+    { id: 6, name: "link6", url: "https://www.google.com" },
+  ];
+
   return (
     <footer>
-      <div className="credits">
+      <div className="footer-top">
+        <div className="section-one">
+          <p>LOGO</p>
+          <p>Coalition of International Students</p>
+          <div className="social-link">Social</div>
+          <div className="social-link">Social</div>
+          <div className="social-link">Social</div>
+          <div className="social-link">Social</div>
+          <p>RM 225J, Student Pavilion,</p>
+          <p>400 E Orange St,</p>
+          <p>Tempe, Arizona 85281.</p>
+        </div>
+        <div className="section-two">
+          <h1>Section title</h1>
+          {links.map(link => (
+            <Link key={link.id} to={link.url}>
+              {link.name}
+            </Link>
+          ))}
+        </div>
+        <div className="section-three">
+          <h1>Section title</h1>
+          {links.map(link => (
+            <a key={link.id} href={link.url}>
+              {link.name}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="footer-bottom">
         <p>
           Arizona State University © Coalition of International Students{" "}
           {date.getFullYear()}. All rights reserved
